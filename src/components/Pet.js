e99daf581c7cd01672f444b8870a272acf400638
Khai Truong <kminht@gmail.com>
2023-03-08 11:22:05 -0500

    added components to test

---
+++ src/components/Pet.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Pet = ()=> {
+    return(
+
+        // React.createElement("div", {}, [
+        // React.createElement("h1", {}, "ginger"),
+        // React.createElement("p", {}, "breed: Crittany Spaniel")
+       // ])
+
+       // code above is the the same as the code below ,just in JSX
+       
+        <div>
+            <h1>Ginger</h1>
+            <p> breed: brritttany Spaniel</p>
+        </div>
+    )
+}
+
+export default Pet;
