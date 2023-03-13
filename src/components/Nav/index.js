3fd32265d665a84552c13b84dccec7cbdfc74dc8
Khai Truong <kminht@gmail.com>
2023-03-13 11:28:22 -0400

    updated nav component

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,26 @@
 import React from "react";
 
 function Nav() {
+  const categorires = [
+    {
+      name: "Commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    {
+      name: "Portraits",
+      decription: "portraits of people in my life",
+    },
+    {
+      name: "Food",
+      description: "Delicious delicacies",
+    },
+    {
+      name: "Landscape",
+      decription: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ];
+
   return (
     <header>
       <h2>
@@ -13,16 +33,20 @@ function Nav() {
       </h2>
 
       <nav>
-          <ul className="flex-row">
-              <li className="mx-2">
-                  <a href="about">
-                      About me
-                  </a>
-              </li>
-              <li>
-                  <span>Contact</span>
-              </li>
-          </ul>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="about">About me</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+
+          {categorires.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span>{category.name}</span>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
